61e042c628ce8bed44cad9206f374b3d188e0c19
Fahmiali99 <79827631+Fahmiali99@users.noreply.github.com>
2021-09-13 04:04:55 +0700

    isi konten hampir selesai

--- src/App.js
+++ src/App.js
@@ -67,30 +67,85 @@ function App() {
 
           <div class="col-6 p-md-3 space-atas tengah">
             <div class="card card-light content-warna">
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
+              <div class="cointainer p-md-3">
+                <h1>What's React?</h1>
+                <hr></hr>
+                <div class="cointainer text-center ">
+                  <img
+                    class="react"
+                    src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+                  ></img>
+                </div>
+
+                <h6>
+                  ReactJS offers graceful solutions to some of front-end
+                  programming’s most persistent issues, allowing you to build
+                  dynamic and interactive web apps with ease. It’s fast,
+                  scalable, flexible, powerful, and has a robust developer
+                  community that’s rapidly growing. There’s never been a better
+                  time to learn React.
+                  <a
+                    class="see-more"
+                    href="https://www.codecademy.com/learn/react-101?g_network=g&g_device=c&g_adid=518718871323&g_keyword=%2Breact.js&g_acctid=243-039-7011&g_adtype=search&g_adgroupid=126771679424&g_keywordid=kwd-302016649835&g_campaign=ROW+Language%3A+Basic+-+Broad&g_campaignid=10947274266&utm_id=t_kwd-302016649835:ag_126771679424:cp_10947274266:n_g:d_c&utm_term=%2Breact.js&utm_campaign=ROW%20Language%3A%20Basic%20-%20Broad&utm_source=google&utm_medium=paid-search&utm_content=518718871323&hsa_acc=2430397011&hsa_cam=10947274266&hsa_grp=126771679424&hsa_ad=518718871323&hsa_src=g&hsa_tgt=kwd-302016649835&hsa_kw=%2Breact.js&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gclid=CjwKCAjwyvaJBhBpEiwA8d38vLfOxmaSelMqayphde5iWeUrDKOxZtMaQ8mTtn7xOz9y6TIAmUVguhoC_boQAvD_BwE"
+                  >
+                    See more
+                  </a>
+                </h6>
+              </div>
             </div>
 
             <div class="card card-light mt-3 content-warna">
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
+              <div class="cointainer p-md-3">
+                <h1>How to use Laravel?</h1>
+                <hr></hr>
+                <div class="cointainer text-center mb-3">
+                  <img
+                    class="laravel"
+                    src="https://www.duniailkom.com/wp-content/uploads/2020/03/Laravel-Featured-Image.png"
+                  ></img>
+                </div>
+
+                <h6>
+                  Laravel is a web application framework with expressive,
+                  elegant syntax. We believe development must be an enjoyable
+                  and creative experience to be truly fulfilling. Laravel
+                  attempts to take the pain out of development by easing common
+                  tasks used in most web projects.
+                  <a class="see-more" href="https://laravel.com/">
+                    See more
+                  </a>
+                </h6>
+              </div>
             </div>
 
             <div class="card card-light mt-3 content-warna">
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
-              <h1>Hello h1</h1>
+              <div class="cointainer p-md-3">
+                <h1>How to be a Frontend Developer?</h1>
+                <hr></hr>
+                <div class="cointainer text-center ">
+                  <img
+                    class="frontend"
+                    src="https://cdn-ak.f.st-hatena.com/images/fotolife/s/serip39/20200715/20200715002753.png?changed=1594740743"
+                  ></img>
+                </div>
+
+                <h6>
+                  Vue.js and React.js are two frontend JavaScript frameworks
+                  popular in the developer world, with React taking the top spot
+                  on the podium for three consecutive years now. Both tools
+                  offer developers a productive approach to building various Web
+                  applications, but each has its own best use cases and responds
+                  to different business needs. In this article, we’ll explore
+                  what those use cases can be, along with perks that make Vue.js
+                  and React unique.
+                  <a
+                    class="see-more"
+                    href="https://www.monterail.com/blog/vue-vs-react-2021"
+                  >
+                    See more
+                  </a>
+                </h6>
+              </div>
             </div>
           </div>
 
